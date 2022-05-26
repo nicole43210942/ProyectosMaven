58dca2d333592e9150a24d30b895447ab234b7ad
Maria M. Martinez <Maria M. Martinez@Maria-Martinez>
2022-05-26 01:10:26 -0300

    mi segundo commit

---
+++ ejemplo/src/main/java/com/mamani/demo/App.java
@@ -0,0 +1,15 @@
+package com.mamani.demo;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JOptionPane.showMessageDialog( null,"hola nicole" );
+    }
+}
